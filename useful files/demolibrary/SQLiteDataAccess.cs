9ff8d9c5cf13bda3c8da82400751456281f1699d
JHurst97 <37871254+JHurst97@users.noreply.github.com>
2020-10-19 21:12:27 +0100

    Add files via upload

---
+++ useful files/demolibrary/SQLiteDataAccess.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace DemoLibrary
+{
+    public class SQLiteDataAccess
+    {
+
+        public static List<PersonModel> LoadPeople()
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<PersonModel>("SELECT * FROM PEOPLE", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+
+        public static void AddPeople(PersonModel person)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute("insert into People (FirstName, LastName) values (@FirstName, @LastName)", person);
+            }
+        }
+
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
---
+++ useful files/form/Form1.cs
@@ -0,0 +1,49 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLiteDemo
+{
+    public partial class Form1 : Form
+    {
+
+        List<PersonModel> people = new List<PersonModel>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            LoadPeople();
+        }
+
+        public void LoadPeople()
+        {
+            people = SQLiteDataAccess.LoadPeople();
+            WireUpList();
+        }
+
+        public void WireUpList()
+        {
+            listBox.DataSource = null;
+            listBox.DataSource = people;
+            listBox.DisplayMember = "FullName";
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            SQLiteDataAccess.AddPeople(new PersonModel { FirstName = firstNameTb.Text, LastName = lastNameTb.Text });
+        }
+
+        private void Refresh_Click(object sender, EventArgs e)
+        {
+            LoadPeople();
+        }
+    }
+}
